5d3d5a7d3d2d64317a86721cb568ec8212be1a25
activityworkshop <mail@activityworkshop.net>
2020-05-08 19:36:26 +0200

    Show incoming contact request messages

--- murmeli/pages/messages.py
+++ murmeli/pages/messages.py
@@ -1,19 +1,35 @@
 '''Module for the messages pageset'''
 
 from murmeli.pages.base import PageSet
+from murmeli.pagetemplate import PageTemplate
+from murmeli import inbox
 
 
 class MessagesPageSet(PageSet):
     '''Messages page set, for showing list of messages etc'''
     def __init__(self, system):
         PageSet.__init__(self, system, "messages")
+        self.messages_template = PageTemplate('messages')
 
     def serve_page(self, view, url, params):
         '''Serve a page to the given view'''
         print("Messages serving page", url, "params:", params)
-        page_title = self.i18n("messages.title") or "<messages>"
-        page_body = "<p>Messages page: '%s'</p><p><a href='/'>[back]</a></p>" % url
-        contents = self.build_page({'pageTitle':page_title,
-                                    'pageBody':page_body,
+        database = self.system.get_component(self.system.COMPNAME_DATABASE)
+        message_list = database.get_inbox() if database else []
+        conreqs = []
+        for msg in message_list:
+            if not msg or msg.get(inbox.FN_DELETED):
+                continue
+            if msg.get(inbox.FN_MSG_TYPE) == "contactrequest":
+                conreqs.append(msg)
+        num_msgs = len(conreqs)
+        bodytext = self.messages_template.get_html(self.get_all_i18n(),
+                                                   {"contactrequests":conreqs,
+                                                    "contactresponses":[],
+                                                    "mails":[], "nummessages":num_msgs,
+                                                    "searchterm":'',
+                                                    "webcachedir":self.get_web_cache_dir()})
+        contents = self.build_page({'pageTitle':self.i18n("messages.title"),
+                                    'pageBody':bodytext,
                                     'pageFooter':"<p>Footer</p>"})
         view.set_html(contents)
